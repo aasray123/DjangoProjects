d61438b4949f0043ddf67b7e64c246d58fc11f97
Aasray Sundar <AXS2175@student.bham.ac.uk>
2025-01-04 16:05:38 +0400

    Comments and cleaning up code

--- Calculator/Test/views.py
+++ Calculator/Test/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import HttpResponse, render, redirect
 from django.template import loader
 
 def home(request):
+    #First time joining or var not made, then init blank
     if ('tempExp' not in request.session) or request.method == 'GET' :
         request.session['tempExp'] = ''
     result = ''
 
     if request.method == 'POST':
-    
         expression = request.POST.get('expression')
+
+        #Calculate final equation
         if(expression == '='):
             try:
                 result = eval(request.session['tempExp'])  # CAUTION: Use eval carefully
@@ -16,8 +18,10 @@ def home(request):
             except Exception as e:
                 result = str(e)
         else:
+            #UPDATE the tempExp value 
             request.session['tempExp'] = request.session['tempExp'] + expression
-            result = request.session['tempExp']
+
+            #DEBUGGING 
             print(request.session['tempExp'])
         
     return render(request, 'index.html', {'result': result })
