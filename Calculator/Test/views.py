1df2c683ddd937c387f60201e04146a21b4763a2
Aasray Sundar <AXS2175@student.bham.ac.uk>
2025-01-04 15:53:27 +0400

    Adding buttons and some code

--- Calculator/Test/views.py
+++ Calculator/Test/views.py
@@ -1,4 +1,20 @@
-from django.http import HttpResponse
+from django.shortcuts import HttpResponse, render, redirect
+from django.template import loader
 
 def home(request):
-    return HttpResponse("SJDKSAJLKDJASLKDAJSD")
+    if ('tempExp' not in request.session) :
+        request.session['tempExp'] = ''
+    result = ''
+
+    if request.method == 'POST':
+        expression = request.POST.get('expression')
+        if(expression == '='):
+            try:
+                result = eval(request.session['tempExp'])  # CAUTION: Use eval carefully
+            except Exception as e:
+                result = str(e)
+        else:
+            request.session['tempExp'] = request.session['tempExp'] + expression
+            print(request.session['tempExp'])
+        
+    return render(request, 'index.html', {'result': result })
