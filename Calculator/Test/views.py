53ad5698bf510c39341723614f034dcb3326c891
Aasray Sundar <AXS2175@student.bham.ac.uk>
2025-01-04 16:21:20 +0400

    Minor Syntax fixes in text field

--- Calculator/Test/views.py
+++ Calculator/Test/views.py
@@ -4,8 +4,8 @@ from django.template import loader
 def home(request):
     #First time joining or var not made, then init blank
     if ('tempExp' not in request.session) or request.method == 'GET' :
-        request.session['tempExp'] = ''
-    result = ''
+        request.session['tempExp'] = '0'
+    result = '0'
 
     if request.method == 'POST':
         expression = request.POST.get('expression')
@@ -17,10 +17,20 @@ def home(request):
                 request.session['tempExp'] = str(result)
             except Exception as e:
                 result = str(e)
+        #Decimal point handling
+        elif(expression == '.'):
+            if(request.session['tempExp'][-1] not in "+-*/."):
+                request.session['tempExp'] += expression 
+            result = request.session['tempExp']   
+
         else:
+            #Clear base 0
+            if(len(request.session['tempExp']) == 1 and request.session['tempExp'][-1] == '0'):
+                request.session['tempExp'] = ''
+
             #UPDATE the tempExp value 
             request.session['tempExp'] = request.session['tempExp'] + expression
-
+            result = request.session['tempExp']
             #DEBUGGING 
             print(request.session['tempExp'])
         
